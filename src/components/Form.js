7eca3201fbae8696e1597c7266931c3cc32c95cc
essencedev9 <essencedev9@nntuple.com>
2021-08-13 04:44:25 +0900

    ADd useRef Sample

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components/native';
 import Button from './Button';
 
@@ -18,24 +18,45 @@ const StyledText = styled.Text`
 const Form = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-  let count = 0;
+
+  const refName = useRef(null);
+  const refEmail = useRef(null);
 
   useEffect(() => {
     console.log('=-=-=-=-=-=-=-= Mount =-=-=-=-=-=-=-=');
+    refName.current.focus();
     return () => console.log('=-=-=-=-=-=-=-= Unmount =-=-=-=-=-=-=-=');
-  });
+  }, []);
 
   useEffect(() => {
     console.log(`name: ${name}, email: ${email}`);
     //console.log(`count: ${count}`);
   }, [email]);
 
+  const onSubmit = () => {
+    console.log('submit');
+  };
+
   return (
     <>
       <StyledText> Name : {name}</StyledText>
       <StyledText> Email : {email}</StyledText>
-      <StyledInput value={name} onChangeText={setName} placeholder="name" />
-      <StyledInput value={email} onChangeText={setEmail} placeholder="email" />
+      <StyledInput
+        value={name}
+        onChangeText={setName}
+        placeholder="name"
+        ref={refName}
+        returnKeyType="next"
+        onSubmitEditing={() => refEmail.current.focus()}
+      />
+      <StyledInput
+        value={email}
+        onChangeText={setEmail}
+        placeholder="email"
+        ref={refEmail}
+        returnKeyType="done"
+        onSubmitEditing={onSubmit}
+      />
       <Button title="count" onPress={() => count + 1} />
     </>
   );
